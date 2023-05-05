12dfa3fb4a51eae1378dc8acab4dbacec00a10d6
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-05-04 20:53:41 -0700

    Plated with task object

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from task import Task
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello Better to do list"
+
+task1 = Task("Hello", "Date")
+
+print("Original:")
+print (task1._name)
+print (task1.date)
+print (task1.estimated_time_to_finish)
+print (task1.notes)
+
+print()
+
+print("Name Changed:")
+task1.set_name("Poop")
+print(task1._name)
+
+print(task1)
