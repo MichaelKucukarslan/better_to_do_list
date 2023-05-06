5f0254e4a0010818e2b704fa7a2821f238485946
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-05-06 14:30:08 -0700

    Started the page to add tasks
    Was able to get the page to start with the users current local date and time
    The user can change this time if they so choose.

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
 from task import Task
-
+from datetime import datetime
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
-def home():
-    return "Hello Better to do list"
+@app.route('/')
+def index():
+   session.clear()
+   return redirect("/add_task")
 
-task1 = Task("Hello", "Date")
+@app.route('/add_task', methods=["GET", "POST"])
+def add_task():
+   if request.method == "GET":
+    date = datetime.now()
+    # print(date)
+    return render_template("add_task.html", date=date)
+   
+   name = request.form.get("name")
+   description = request.form.get("description")
+   date = request.form.get("date")
+   estimated_time_to_finish = request.form.get("date")
 
-print("Original:")
-print (task1._name)
-print (task1.date)
-print (task1.estimated_time_to_finish)
-print (task1.notes)
+   task = Task(name, date, estimated_time_to_finish, description)
+   print(task)
+   return redirect("/add_task")
 
-print()
-
-print("Name Changed:")
-task1.set_name("Poop")
-print(task1._name)
-
-print(task1)
